8f1e5bde54ff43194a9a539f94a71c6f43b7af38
rojamich <rojamich@oregonstate.edu>
2024-05-08 19:13:04 -0700

    set up non functional

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "index.html"))});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Number Converter listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -54,7 +55,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Nope!
     </section>
   </body>
 </html>
