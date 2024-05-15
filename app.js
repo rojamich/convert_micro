5f66218a5cfb4e2a3c82b035056a58e981f86f9e
rojamich <rojamich@oregonstate.edu>
2024-05-15 12:17:51 -0700

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const path = require("path");
 const port = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "index.html"))});
 
